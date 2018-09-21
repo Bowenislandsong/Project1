ad8ff82a046fd48bb22d39dcada8393fb58b3ea4
lihaooo233 <43101480+lihaooo233@users.noreply.github.com>
2018-09-21 22:54:00 +0800

    Add files via upload
    maybe the final version

--- tweepy-use.py
+++ tweepy-use.py
@@ -12,21 +12,29 @@ from PIL import Image, ImageDraw, ImageFont
 def add_text(u,fn):
 
     im = Image.open('images/'+fn+'.jpg').convert('RGBA')
-    im=im.resize((900, 1200))
-    words = Image.new('RGBA', im.size, (255, 255, 255, 0))
+    min_size =1000
+    fill_color = (0, 0, 0, 0)
+    x, y = im.size
+    size = max(min_size, x, y)
+    new_im = Image.new('RGBA', (size, size), fill_color)
+    a = int(size - x)
+    b = int(size - y)
+    new_im.paste(im, (int(a / 2), int(b / 2)))
+
+    words = Image.new('RGBA', new_im.size, (255, 255, 255, 0))
     fnt = ImageFont.truetype('arial.ttf', 60)
     d = ImageDraw.Draw(words)
-    # draw text, half opacity
-    xx=1
-    c=im.size[1]/50
-    c=int(c)
+    # draw text
+    xx = 1
+    c = im.size[1] / 50
+    c = int(c)
 
     for i in u:
         if(xx>c):
             break
 
         d.text((100,xx*40 ), i, font=fnt, fill=(235, 21, 200, 255))
-        out = Image.alpha_composite(im, words)
+        out = Image.alpha_composite(new_im, words)
         xx=xx+1
 
     im=out.convert("RGB")
@@ -37,7 +45,9 @@ def add_text(u,fn):
 
 def google_vision(figure_number,num):
     # verify the API
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google.json"
     verify_api = vision.ImageAnnotatorClient()
+
     # add some images
     filepath = "images"
     fn=str(figure_number)
@@ -71,94 +81,79 @@ def API_verify():
     return (api)
 
 def Twitter_Photos(api):
-    #tweets = api.home_timeline()
+
     name = input('please input user screen name:')
-    tweets=api.user_timeline(screen_name=name)
 
 
-    tweets = api.user_timeline(screen_name=name, count=40)
+    try:
+        tweets = api.user_timeline(screen_name=name)
+        tweets = api.user_timeline(screen_name=name, count=40)
 
-    url = []
-    for items in tweets:
+        url = []
+        for items in tweets:
 
-        if items.entities.__contains__('media') == 0:
-            continue
-
-        media_class = items.extended_entities.get('media')
-        for sth in media_class:
-            if sth['type'] != 'photo':
+            if items.entities.__contains__('media') == 0:
                 continue
 
-            url.append(sth['media_url'])
+            media_class = items.extended_entities.get('media')
+            for sth in media_class:
+                if sth['type'] != 'photo':
+                    continue
 
-    for i in url:
+                url.append(sth['media_url'])
 
-        photos = wget.download(i)
-        # equest.urlretrieve(i,'image',)
-        file = open('tweet.txt', 'w')
-        for status in tweets:
-            json.dump(status._json, file, sort_keys=True, indent=4)
-        file.close()
-    x = 1
-    num = len(url)
+        for i in url:
 
-    if os.path.exists('images') == 0:
-        os.makedirs('images', mode=0o777)
-    for files in os.listdir():
+            photos = wget.download(i)
+            file = open('tweet.txt', 'w')
+            for status in tweets:
+                json.dump(status._json, file, sort_keys=True, indent=4)
+            file.close()
+        x = 1
+        num = len(url)
 
-        if files == 'tweepy-use.py':
-            continue
-        if files == 'tweet.txt':
-            continue
-        if files == 'images':
-            continue
-        if files == 'arial.ttf':
-            continue
-        if files == 'video_image':
-            continue
-        if files == 'outcome.mp4':
-            continue
-        if files == 'project1':
-            continue
-        if files == 'README.md':
-            continue
+        if os.path.exists('images') == 0:
+            os.makedirs('images', mode=0o777)
+        for files in os.listdir():
+            search = list(files)
+            if search[-1] == 'g' and search[-2] == 'p' and search[-3] == 'j':
+                if num < 10:
+                    x = str(x)
+                    if os.path.exists('images/' + x + '.jpg') == 0:
+                        os.rename(files, x + '.jpg')
+                    else:
+                        os.remove('images\\' + x + '.jpg')
+                        os.rename(files, x + '.jpg')
+                    shutil.move(x + '.jpg', "images")
 
-        if num < 10:
-            x = str(x)
-            # os.rename(files,'0'+x+'.jpg')
-            if os.path.exists('images/'+x+'.jpg') == 0:
-
-                os.rename(files, x + '.jpg')
-            else :
-                os.remove('images\\'+x+'.jpg')
-
-                os.rename(files, x + '.jpg')
-            # shutil.move("D:\\photos\\"+'0'+x+'.jpg',"D:\\photos\\images")
-            shutil.move(  x + '.jpg', "images")
-
-        else:
-            if x < 10:
-                x = str(x)
-                if os.path.exists('images\\'+'0'+x+'.jpg') == 0:
-                    os.rename(files, '0' + x + '.jpg')
                 else:
-                    os.remove('images\\' +'0'+ x + '.jpg')
-                    os.rename(files, '0' + x + '.jpg')
-                # os.rename(files, x + '.jpg')
-                shutil.move('0' + x + '.jpg', "images")
-                #shutil.move("D:\\photos\\" + x + '.jpg', "D:\\photos\\images")
-            else:
-                x = str(x)
-                if os.path.exists('images\\' + x + '.jpg') == 0:
-                    os.rename(files, x + '.jpg')
-                else:
-                    os.remove('images\\' + x + '.jpg')
-                    os.rename(files, x + '.jpg')
-                shutil.move( x + '.jpg', "images")
+                    if x < 10:
+                        x = str(x)
+                        if os.path.exists('images\\' + '0' + x + '.jpg') == 0:
+                            os.rename(files, '0' + x + '.jpg')
+                        else:
+                            os.remove('images\\' + '0' + x + '.jpg')
+                            os.rename(files, '0' + x + '.jpg')
+                        shutil.move('0' + x + '.jpg', "images")
+                    else:
+                        x = str(x)
+                        if os.path.exists('images\\' + x + '.jpg') == 0:
+                            os.rename(files, x + '.jpg')
+                        else:
+                            os.remove('images\\' + x + '.jpg')
+                            os.rename(files, x + '.jpg')
+                        shutil.move(x + '.jpg', "images")
 
-        x = int(x)
-        x = x + 1
-    return (num)
+
+
+                x = int(x)
+                x = x + 1
+        return (num)
+
+
+
+    except:
+        print('seems nobody has this name.... or something unknown happened')
 
 
 
@@ -181,16 +176,19 @@ if os.path.exists('images') == 1:
 else:
     os.makedirs('images', mode=0o777)
 num=Twitter_Photos(api)
+try:
+    for i in range(num):
+        f = i + 1
+        google_vision(f, num)
 
-for i in range(num):
-    f=i+1
-    google_vision(f,num)
+    if os.path.exists('outcome.mp4') == 1:
+        os.remove('outcome.mp4')
 
-if os.path.exists('video.mp4') == 1:
-    os.remove('video.mp4')
+    if num < 10:
+        os.system("ffmpeg -y -r 1 -i video_image\%01d.jpg  -vcodec libx264 -r 1 -t 15 -b 200k outcome.mp4")
 
-if num<10:
-    os.system("ffmpeg -y -r 1 -i video_image\%01d.jpg  -vcodec libx264 -r 1 -t 15 -b 200k outcome.mp4")
-
-else:
-    os.system("ffmpeg -y -r 1 -i video_image\%02d.jpg -vcodec libx264 -r 1 -t 15 -b 200k outcome.mp4")
+    else:
+        os.system("ffmpeg -y -r 1 -i video_image\%02d.jpg -vcodec libx264 -r 1 -t 15 -b 200k outcome.mp4")
+    print('you can find the result from current folder \(^-^)/ ')
+except:
+    print('Maybe...this account have too much photos or google credential problem.')
